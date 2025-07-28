f769ae2c3474c0bcb3b218b48d94793f511ce8cd
nickly24 <nikolaypribysh@mail.ru>
2025-07-28 13:33:48 +0300

    add balance

--- src/Config.js
+++ src/Config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://nickly24-cpm-serv-0fe5.twc1.net';
-export const API_EXAM_URL = 'https://nickly24-cpm-exam-main-abb6.twc1.net';
+export const API_BASE_URL = 'https://main.api.cpm-lms.ru';
+export const API_EXAM_URL = 'https://exam.api.cpm-lms.ru';
